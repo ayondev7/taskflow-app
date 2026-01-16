ed565613eff3315b8ff943df4fdd52d3248b2520
Abdur Rahman Ayon <abdurrahmanayon66@gmail.com>
2026-01-16 17:59:38 +0600

    update

---
+++ backend-express-ts/src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app= express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on http://localhost:${PORT}`);
+})
